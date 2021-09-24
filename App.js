e1f182a3c1373ef6927dbab7779516d088a5c555
ehsaasgrover <61815658+ehsaasgrover@users.noreply.github.com>
2021-09-24 18:58:00 +1200

    [003] Playing around with flexes

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
     >
       <View style={{
         backgroundColor: "dodgerblue",
-        flex: 2,
+        flex: 1,
       }}/> 
       <View style={{
         backgroundColor: "gold",
