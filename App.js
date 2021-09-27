00858a769b805e84fbb7a4d05a3a53da202db5b0
ehsaasgrover <61815658+ehsaasgrover@users.noreply.github.com>
2021-09-27 17:44:49 +1300

    [005] Formatting

--- App.js
+++ App.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import {View, Button} from 'react-native';
 
 export default function App() {
-  
-  return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-    }}
-    >
-      <View style={{
-        backgroundColor: "dodgerblue",
-        flex: 1,
-      }}/> 
-      <View style={{
-        backgroundColor: "gold",
-        flex: 1,
-      }}/> 
-      <View style={{
-        backgroundColor: "tomato",
-        flex: 1,
-      }}/> 
-    </View>
-    // Video upto 1:30:55
-  );
+
+    return (
+        <View style={{
+            backgroundColor: "#fff",
+            flex: 1,
+        }}>
+            <View style={{
+                backgroundColor: "dodgerblue",
+                flex: 1,
+            }}/>
+
+            <Button
+                color="red"
+                title="Click Me"
+                onPress={() => alert("Button tapped")}
+            />
+
+            <View style={{
+                backgroundColor: "gold",
+                flex: 1,
+            }}/>
+
+            <View style={{
+                backgroundColor: "tomato",
+                flex: 1,
+            }}/>
+        </View>
+    );
 }
 
  
