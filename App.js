7c1b30716071f0356e414fd13e1999949800765a
ehsaasgrover <61815658+ehsaasgrover@users.noreply.github.com>
2021-09-30 14:18:51 +1300

    [006] Creating the medication app header and working on the alert reminder view

--- App.js
+++ App.js
@@ -1,35 +1,45 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {Text, SafeAreaView, View, StyleSheet} from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.background}>
+                <View style={styles.medicationHeaderContainer}>
+                    <Text style={styles.medicationHeader}> Medication </Text>
+                </View>
+                <View style={styles.reminderAlertContainer}/>
+            </View>
+        );
+    }
 
-    return (
-        <View style={{
-            backgroundColor: "#fff",
-            flex: 1,
-        }}>
-            <View style={{
-                backgroundColor: "dodgerblue",
-                flex: 1,
-            }}/>
-
-            <Button
-                color="red"
-                title="Click Me"
-                onPress={() => alert("Button tapped")}
-            />
-
-            <View style={{
-                backgroundColor: "gold",
-                flex: 1,
-            }}/>
-
-            <View style={{
-                backgroundColor: "tomato",
-                flex: 1,
-            }}/>
-        </View>
-    );
 }
 
- 
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    medicationHeaderContainer: {
+        marginTop: 40,
+        height: "10%",
+        backgroundColor: "dodgerblue"
+    },
+    medicationHeader: {
+        fontSize: 40,
+        bottom: -30
+    },
+
+    reminderAlertContainer: {
+        marginTop: 5,
+        height: '8%',
+        width: '90%',
+        backgroundColor: "dodgerblue",
+        borderBottomStartRadius: 20,
+        borderBottomEndRadius: 20,
+        borderTopStartRadius: 20,
+        borderTopEndRadius: 20
+    }
+
+});
